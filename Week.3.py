b588dca6e61aac7d19c6ffbd734ae47e91d1d110
B00164287 Katarzyna Dzhumal <B00164287@mytudublin.ie>
2026-02-09 12:46:31 +0000

    Completed Assignment

---
+++ Week.3.py
@@ -0,0 +1,16 @@
+
+#Katarzyna Dzhumal B00164287
+#09/02/2026
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps(a,b):
+    for i in range(a,b+1):
+        print(f"Step {i}")
+
+
+def main():
+    printSteps(1,9)
+
+if __name__ =="__main__":
+    main()
+  
